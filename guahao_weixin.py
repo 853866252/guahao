03341451e586259fa802a0b80cf14316f9f18d77
853866252 <754121698@qq.com>
2017-07-06 11:23:54 +0800

    1
    1

--- guahao_weixin.py
+++ guahao_weixin.py
@@ -3,8 +3,16 @@ import werobot
 robot = werobot.WeRoBot(token='ce1Jcs')
 
 @robot.text
-def hello_world():
-    return 'Hello World!'
+def hello_world(message):
+    return message.content()
+
+
+#@robot.text
+#def first(message, session):
+#    if 'first' in session:
+#        return '你之前给我发过消息'
+#    session['first'] = True
+#    return '你之前没给我发过消息'
 
 robot.config['HOST'] = '172.17.76.183'
 robot.config['PORT'] = 80
