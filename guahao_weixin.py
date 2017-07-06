463d4219216913213e5c63f521e3843c32547865
853866252 <754121698@qq.com>
2017-07-06 11:25:46 +0800

    11

--- guahao_weixin.py
+++ guahao_weixin.py
@@ -7,13 +7,6 @@ def hello_world(message):
     return message.content()
 
 
-#@robot.text
-#def first(message, session):
-#    if 'first' in session:
-#        return '你之前给我发过消息'
-#    session['first'] = True
-#    return '你之前没给我发过消息'
-
 robot.config['HOST'] = '172.17.76.183'
 robot.config['PORT'] = 80
 
