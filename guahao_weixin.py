c4850d02973a8fbe77aea1fddb6be13aaddc1036
853866252 <754121698@qq.com>
2017-07-10 19:20:31 +0800

    1

--- guahao_weixin.py
+++ guahao_weixin.py
@@ -3,10 +3,42 @@
 import werobot
 import sqlite3
 from werobot import WeRoBot
+import hashlib
 import requests
-
+import re
+import urllib2
+import pymongo
 robot = werobot.WeRoBot(token='ce1Jcs')
 
+def get_verify():
+    url = 'http://book.xachyy.com/modules/verifyImage.ashx'
+    session = requests.session()
+    response_headers = session.get(url).headers
+    session_id = ''.join(re.findall('ASP.NET_SessionId=(.*); path=/;',response_headers['Set-Cookie']))
+    code_id = ''.join(re.findall('HBHOSPITALCODE=(\d\d\d\d)',response_headers['Set-Cookie']))
+    return session_id,code_id
+
+def get_patientId(weixin_session,session_id,code_id,indentify_id,password):
+    client = pymongo.MongoClient()
+    database = client.Patient
+    col = database.doctor_info
+    patient = {}
+    i = 'ASP.NET_SessionId={session}; HBHOSPITALCODE={code}'.format(session=session_id,code=code_id)
+    cookie = {'Cookie': i}
+    print cookie
+    url = 'http://book.xachyy.com/passport/SsoLogin.aspx?user={user}&pwd={pwd}&app=0&loginType=2-1&hospitalId=61010021&verifycode={code}'.format(user=indentify_id,pwd=password,code=code_id)
+    print url
+    opener = urllib2.build_opener()
+    opener.addheaders.append(('Cookie',i))
+    f = opener.open(url)
+    a = re.findall('"Accountid":"(.*)","Accountname', f.read())
+    if a:
+        patient['session'] = weixin_session
+        patient['Accoutid'] = a[0]
+        col.insert(patient)
+        return "登录成功"
+    else:
+        return "登录用户名或密码错误，请重新输入"
 
 @robot.subscribe
 def intro(message):
@@ -19,7 +51,11 @@ def hello(message, session):
     print task
     type(task[0])
     if task[0].encode('utf-8') == '登录':
-        return "1"
+        session_id, code_id = get_verify()
+        identify_id = task[1].encode('utf-8')
+        password = hashlib.md5(task[2].encode('utf-8'))
+        back = get_patientId(message.source,session_id, code_id, identify_id, password)
+        return back
 
     elif task[0].encode('utf-8') == '挂号':
         return "2"
