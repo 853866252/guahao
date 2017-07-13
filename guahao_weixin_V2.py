4aaa8127fef68fa8cd8b9e0cc482733da1918f6c
853866252 <754121698@qq.com>
2017-07-13 11:44:27 +0800

    5

--- guahao_weixin_V2.py
+++ guahao_weixin_V2.py
@@ -96,8 +96,8 @@ def get_verify_register(session):
             print type(date_time)
             if date_time == {}:
                 col4.insert(task)
-                col1.delete_one({'Session': session})
-                return "没有开始或者已经预定完,已经为您转为明日抢号，取消请输入3"
+
+                return "没有开始或者已经预定完,已经为您转为明日抢号，请耐心等待。"
             back = register(patientinfo, doctor_info, date_time)
             col1.delete_one({'Session': session})
             return back
