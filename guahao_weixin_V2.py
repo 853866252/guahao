24c111797e7f65d551b85b4e164919ec8070019d
853866252 <754121698@qq.com>
2017-07-25 08:50:21 +0800

    1

--- guahao_weixin_V2.py
+++ guahao_weixin_V2.py
@@ -121,6 +121,7 @@ def get_book_items(doctor_info,URL):
     date1 = str(datetime.date.today().replace(day=1))
     date2 = str(datetime.date.today().replace(day=1) - datetime.timedelta(-31))
     datelist = [date1,date2]
+    print datelist
     date_time = {}
     for each in datelist:
         url = 'http://'+URL+'/Doctor/ajax.aspx?param=GetBookInfoByDoctorId&uimode=1&clinicLabelId='+doctor_info['ClinicLabelId'].encode("utf-8")+'&cliniclabeltype=2&clinicweektype=0&rsvmodel=1&doctorid='+doctor_info['DoctorID'].encode("utf-8")+'&selectTime='+each
